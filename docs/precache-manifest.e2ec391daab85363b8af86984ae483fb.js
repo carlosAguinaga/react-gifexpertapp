22a044cac6e83cbf4eff0795e2b8a89e3eb95a40
Carlos <carlosaguinaga01@gmail.com>
2020-08-31 11:06:06 -0500

    docs agregados

---
+++ docs/precache-manifest.e2ec391daab85363b8af86984ae483fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd71715d413e81498396825c213aa31a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fac8a6db76c5ac35ef20",
+    "url": "/static/css/main.28f0bf16.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fac8a6db76c5ac35ef20",
+    "url": "/static/js/main.7270e662.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
